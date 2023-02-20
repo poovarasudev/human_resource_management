d67bd719338ef78910bd26d4cbd945691fd3fa72
Poovarasu <poovarasu@mallow-tech.com>
2023-02-20 08:51:28 +0530

    Added auth redirects to settings file

--- human_resource_management/settings.py
+++ human_resource_management/settings.py
@@ -141,3 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom Settings.
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGIN_URL = '/login'
+LOGOUT_REDIRECT_URL = '/login'
