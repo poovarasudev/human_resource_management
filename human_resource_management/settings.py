98963e08751847a0ff7c7ce71e88cda17409d029
Poovarasu <poovarasu@mallow-tech.com>
2023-02-25 10:30:38 +0530

    Update settings.py

--- human_resource_management/settings.py
+++ human_resource_management/settings.py
@@ -136,11 +136,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+  os.path.join(SITE_ROOT, 'static/'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
